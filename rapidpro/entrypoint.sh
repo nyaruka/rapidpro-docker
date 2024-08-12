da54be362ccaa12ab0acd73bdd757c4f68eb5868
Rowan Seymour <rowanseymour@gmail.com>
2024-08-12 17:02:46 -0500

    Fix celery container

--- rapidpro/entrypoint.sh
+++ rapidpro/entrypoint.sh
@@ -2,5 +2,13 @@
 
 export REMOTE_CONTAINERS=true
 
-poetry run python3 manage.py migrate
-poetry run python3 manage.py runserver 0.0.0.0:8000
+ACTION=${1:-webapp}
+
+if [ "$ACTION" = "webapp" ]; then
+    echo "Running RapidPro webapp..."
+	poetry run python3 manage.py migrate
+    poetry run python3 manage.py runserver 0.0.0.0:8000
+elif [ "$ACTION" = "celery" ]; then
+    echo "Running RapidPro celery worker..."
+	.venv/bin/celery -A temba worker -E -B --loglevel=INFO
+fi
